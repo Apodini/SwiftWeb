b9ad4dface86b50f9992f8a0c8560a71935cd455
Quirin Schweigert <quirin.schweigert@outlook.com>
2020-05-14 20:48:05 +0200

    Remove template content

--- Tests/ApodiniTests/ApodiniTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import Apodini
-
-
-final class ApodiniTests: XCTestCase {
-    func testExample() {
-        XCTAssertEqual(Apodini().text, "Hello, Apodini!")
-    }
-}
--- Tests/SwiftWebTests/SwiftWebTests.swift
+++ Tests/SwiftWebTests/SwiftWebTests.swift
@@ -6,7 +6,7 @@ final class SwiftWebTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SwiftWeb().text, "Hello, World!")
+        // XCTAssertEqual(SwiftWeb().text, "Hello, World!")
     }
 
     static var allTests = [
